fe54241c0b13c41ef5acd0c7b8650e789ee633d4
Juan Pablo <pablo.lopez@iteso.mx>
2023-11-21 12:04:51 -0600

    Syslog server fully functional. Logging logs to different files

--- Log_Analyzer/main.py
+++ Log_Analyzer/main.py
@@ -4,7 +4,25 @@ import time
 
 HOST = "0.0.0.0"
 PORT = 514
-logging.basicConfig(level=logging.INFO, format="%(message)s", filename="cisco_logs.log", filemode="a")
+
+
+def setup_logger(logger_name, log_file, level=logging.INFO):
+    log_setup = logging.getLogger(logger_name)
+    formatter = logging.Formatter('%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    file_handler = logging.FileHandler(log_file, mode='a')
+    file_handler.setFormatter(formatter)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    log_setup.setLevel(level)
+    log_setup.addHandler(file_handler)
+    log_setup.addHandler(stream_handler)
+
+
+setup_logger('log_router1', 'r1.log')
+setup_logger('log_router2', 'r2.log')
+setup_logger('log_router3', 'r3.log')
+setup_logger('log_router4', 'r4.log')
+setup_logger('log_router5', 'r5.log')
 
 
 class SyslogUDPHandler(socketserver.BaseRequestHandler):
@@ -12,16 +30,20 @@ class SyslogUDPHandler(socketserver.BaseRequestHandler):
         data = bytes.decode(self.request[0])
 
         if self.client_address[0] == '1.1.1.1':
-            print(f'Client:Router 1 '+data)
+            log = logging.getLogger('log_router1')
+            log.info(data)
         elif self.client_address[0] == '2.2.2.2':
-            print(f'Client:Router 2 '+data)
+            log = logging.getLogger('log_router2')
+            log.info(data)
         elif self.client_address[0] == '3.3.3.3':
-            print(f'Client:Router 3 '+data)
+            log = logging.getLogger('log_router3')
+            log.info(data)
         elif self.client_address[0] == '4.4.4.4':
-            print(f'Client:Router 4 '+data)
+            log = logging.getLogger('log_router4')
+            log.info(data)
         elif self.client_address[0] == '5.5.5.5':
-            print(f'Client:Router 5 '+data)
-        # logging.info(data)
+            log = logging.getLogger('log_router5')
+            log.info(data)
 
 
 if __name__ == "__main__":
@@ -29,9 +51,12 @@ if __name__ == "__main__":
         print("**Starting Syslog server**\n")
         server = socketserver.UDPServer((HOST, PORT), SyslogUDPHandler)
         time.sleep(1)
-        print("**Port Started Successfully**")
+        print("**Syslog Server Started Successfully**\n")
+        time.sleep(1)
+        print(f"**Listening on port> {PORT}**")
         server.serve_forever(poll_interval=0.5)
     except (IOError, SystemExit):
         raise
     except KeyboardInterrupt:
-        print("**Ctrl+C Pressed. Shutting down the server...**")
+        print("**Ctrl+C Pressed. Shutting down the server...**\n")
+        time.sleep(1)
