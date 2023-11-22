ce1b8572fc2d3d098be96b65b4249aefefaf7afe
Juan Pablo <pablo.lopez@iteso.mx>
2023-11-22 01:32:46 -0600

    Compliance functional with creative part. Missing implementations for other routers

--- Compliance_Management/main.py
+++ Compliance_Management/main.py
@@ -39,51 +39,98 @@ r5 = {
 net_connect = ConnectHandler(**r1)
 loopback1_is_up = False
 loopback1_has_ip = False
+banner_exist = False
+telnet_is_up = False
 print("**Successfully Connected to Router 1**\n")
 time.sleep(1)
-# print("**Confirming that the Loopback1 is Up**\n")
-# time.sleep(1)
-# sh_output = net_connect.send_command("show ip int brief", use_textfsm=True)
-# sh_output_length = len(sh_output)
-# for i in range(0, sh_output_length):
-#     if sh_output[i]['interface'] == 'Loopback1' and sh_output[i]['status'] == 'up':
-#         loopback1_is_up = True
-#     if sh_output[i]['ip_address'] != 'unassigned' and sh_output[i]['interface'] == 'Loopback1':
-#         loopback1_has_ip = True
-# if loopback1_is_up and loopback1_has_ip:
-#     print('**The Loopback1 Interface of the Router 1 is Up and it has an IP Address Assigned**\n')
-# elif not loopback1_is_up and loopback1_has_ip:
-#     print('**The Loopback1 Interface of the Router 1 is Down but it has an IP Address Assigned**\n')
-#     time.sleep(1)
-#     print("**Changing Loopback1 State to Up**\n")
-#     commands = ['interface loopback1', 'no shutdown']
-#     sh_output = net_connect.send_config_set(commands)
-#     time.sleep(1)
-#     print("**Loopback1 is Up**\n")
-# elif loopback1_is_up and not loopback1_has_ip:
-#     print('**The Loopback1 Interface of the Router 1 is Up but the IP Address is Unassigned**\n')
-#     time.sleep(1)
-#     print("**Assigning a new IP Address**\n")
-#     time.sleep(1)
-#     ip_address = input("Enter an IP Address>")
-#     netmask = input("Enter the netmask>")
-#     time.sleep(1)
-#     commands = ['interface loopback1', f'ip address {ip_address} {netmask}']
-#     sh_output = net_connect.send_config_set(commands)
-#     print("**The IP Address has been Assigned**\n")
-# else:
-#     print('**The Loopback1 Interface of the Router 1 is Down and the IP Address is Unassigned**\n')
-#     time.sleep(1)
-#     print("**Changing Loopback1 State to Up and Assigning a new IP Address**\n")
-#     time.sleep(1)
-#     ip_address = input("Enter an IP Address>")
-#     netmask = input("Enter the netmask>")
-#     time.sleep(1)
-#     commands = ['interface loopback1', f'ip address {ip_address} {netmask}', 'no shutdown']
-#     sh_output = net_connect.send_config_set(commands)
-#     print("**Loopback1 is Up and the IP Address has been Assigned**\n")
+
+# telnet
+print("**Checking that Telnet is Disabled**\n")
 time.sleep(1)
-#
+sh_output = net_connect.send_command("show run")
+sh_output_length = len(sh_output)
+for i in range(0, sh_output_length):
+    if sh_output.find('transport input all') != -1 or sh_output.find('transport input telnet') != -1:
+        telnet_is_up = True
+
+if telnet_is_up:
+    print("**Telnet is Enabled**\n")
+    time.sleep(1)
+    print("**Disabling Telnet**\n")
+    time.sleep(1)
+    commands = ['line vty 0 4', 'transport input ssh']
+    sh_output = net_connect.send_config_set(commands)
+    print("**Telnet has been Disabled**\n")
+    time.sleep(1)
+else:
+    print("**Telnet is Disabled. Only SSH Connections are Permitted**\n")
+    time.sleep(1)
+# banner-motd
+print("**Confirming that there is a Message of the Day in Router 1**\n")
+time.sleep(1)
+sh_output = net_connect.send_command("show run")
+sh_output_length = len(sh_output)
+for i in range(0, sh_output_length):
+    if sh_output.find('banner motd') != -1:
+        banner_exist = True
+if not banner_exist:
+    print("**The Message of the Day has not been Set**\n")
+    time.sleep(1)
+    message = input('Enter a short message>')
+    net_connect.config_mode()
+    sh_output = net_connect.send_command(f'banner motd # *****{message}***** #')
+    print("**The Message of the Day has been Set Correctly**\n")
+    time.sleep(1)
+else:
+    print("**The Message of the Day has been Set**\n")
+    time.sleep(1)
+# Loopback1
+print("**Confirming that the Loopback1 is Up**\n")
+time.sleep(1)
+sh_output = net_connect.send_command("show ip int brief", use_textfsm=True)
+sh_output_length = len(sh_output)
+for i in range(0, sh_output_length):
+    if sh_output[i]['interface'] == 'Loopback1' and sh_output[i]['status'] == 'up':
+        loopback1_is_up = True
+    if sh_output[i]['ip_address'] != 'unassigned' and sh_output[i]['interface'] == 'Loopback1':
+        loopback1_has_ip = True
+if loopback1_is_up and loopback1_has_ip:
+    print('**The Loopback1 Interface of the Router 1 is Up and it has an IP Address Assigned**\n')
+    time.sleep(1)
+elif not loopback1_is_up and loopback1_has_ip:
+    print('**The Loopback1 Interface of the Router 1 is Down but it has an IP Address Assigned**\n')
+    time.sleep(1)
+    print("**Changing Loopback1 State to Up**\n")
+    commands = ['interface loopback1', 'no shutdown']
+    sh_output = net_connect.send_config_set(commands)
+    time.sleep(1)
+    print("**Loopback1 is Up**\n")
+    time.sleep(1)
+elif loopback1_is_up and not loopback1_has_ip:
+    print('**The Loopback1 Interface of the Router 1 is Up but the IP Address is Unassigned**\n')
+    time.sleep(1)
+    print("**Assigning a new IP Address**\n")
+    time.sleep(1)
+    ip_address = input("Enter an IP Address>")
+    netmask = input("Enter the netmask>")
+    time.sleep(1)
+    commands = ['interface loopback1', f'ip address {ip_address} {netmask}']
+    sh_output = net_connect.send_config_set(commands)
+    print("**The IP Address has been Assigned**\n")
+    time.sleep(1)
+else:
+    print('**The Loopback1 Interface of the Router 1 is Down and the IP Address is Unassigned**\n')
+    time.sleep(1)
+    print("**Changing Loopback1 State to Up and Assigning a new IP Address**\n")
+    time.sleep(1)
+    ip_address = input("Enter an IP Address>")
+    netmask = input("Enter the netmask>")
+    time.sleep(1)
+    commands = ['interface loopback1', f'ip address {ip_address} {netmask}', 'no shutdown']
+    sh_output = net_connect.send_config_set(commands)
+    print("**Loopback1 is Up and the IP Address has been Assigned**\n")
+    time.sleep(1)
+
 # # Router 2
 # net_connect = ConnectHandler(**r2)
 # print("**Successfully Connected to Router 2**\n")
